647123563a98f835c13867bc9cd85417ca97986b
cmagorian <chrismagorian@gmail.com>
2020-12-24 12:03:21 -0800

    install node 12.x as default version

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -19,7 +19,7 @@ echo "#############################"
 echo "## Setting up BabyPi deps ###"
 echo "#############################"
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt-get install -y nodejs
 
 echo "#############################"
